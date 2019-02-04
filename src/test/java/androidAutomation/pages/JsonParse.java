30a9c6ca99a7c8503cd0e8dbbded9aeb41b0a319
akuzikovv@gmail.com <akuzikovv@gmail.com>
2019-02-04 18:27:13 +0200

    json_password

--- src/test/java/androidAutomation/pages/JsonParse.java
+++ src/test/java/androidAutomation/pages/JsonParse.java
@@ -5,7 +5,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
+
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -16,6 +18,14 @@ public class JsonParse {
     private static URL urlObject;
     private static StringBuffer response;
 
+    @Test
+    public void parseCredentials() throws IOException, ParseException {
+        JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("src/test/resources/files/Credentials.json"));
+        JSONObject accountObject = (JSONObject) parser.parse(jsonObject.get("account").toString());
+        String login = accountObject.get("login").toString();
+        String password = accountObject.get("password").toString();
+        System.out.println("Login: " + login + "\n" + "Password: " + password);
+    }
 
     @Test
     public void parseJson() throws IOException, ParseException {
@@ -38,38 +48,5 @@ public class JsonParse {
         String email = userDetailsObject.get("email").toString();
         String description = timezoneObject.get("description").toString();
         System.out.println("User email: " + email + "\n" + "Description: " + description);
-
     }
-
-//    @Test
-//    public void parseJson2() throws IOException, ParseException {
-//        String url = "https://randomuser.me/api/";
-//        urlObject = new URL(url);
-//        HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
-//        connection.setRequestMethod("GET");
-//        int responseCode = connection.getResponseCode();//get Response Code
-//        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//        String inputLine;
-//        response = new StringBuffer();
-//        while ((inputLine = in.readLine()) != null) {
-//            response.append(inputLine);
-//        }
-//        in.close();
-//        JSONObject jsonObject = (JSONObject) parser.parse(response.toString());
-//        JSONArray resultsObject = (JSONArray) parser.parse(jsonObject.get("results").toString());
-//        JSONObject userDetailsObject = (JSONObject) parser.parse(resultsObject.get(0).toString());
-//        JSONObject locationObject = (JSONObject) parser.parse(userDetailsObject.get("location").toString());
-//        JSONObject timezonObject = (JSONObject) parser.parse(locationObject.get("timezone").toString());
-//
-//        String name = userDetailsObject.get("name").toString();
-//        String email = userDetailsObject.get("email").toString();
-//        String timezone = locationObject.getOrDefault("timezone", 0).toString();
-//        String description = timezonObject.get("description").toString();
-//        System.out.println("\nSending 'GET' request to URL : " + url);
-//        System.out.println("Response Code : " + responseCode);
-//        System.out.println("User name: " + name + "\n" + "User email: " + email + "\n" + "Description: " + description);
-//
-//    }
-
-
 }
